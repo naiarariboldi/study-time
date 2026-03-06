9c6d2aeb914fd34ab9e6473e9178f8ecac29fac5
naiarariboldi <naiara.riboldi@outlook.com>
2026-03-06 19:36:00 -0300

    integração com API do ENEM

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
-async function buscarQuestao(params) {
-    const resposta = await fetch("https://enem.dev/api/v1/exams/2020/questions")
+async function buscarQuestao() {
 
-    const dados = await resposta.json()
+const resposta = await fetch("https://enem.dev/api/v1/exams/2020/questions")
 
-    const questao = dados[0]
+const dados = await resposta.json()
 
-    document.getElementById("questao").innerHTML = 
-    `
-    <h2>${questao.title}</h2>
-    <p>${questao.context}</p>
-    `
+const questao = dados.questions[0]
+
+let alternativas = ""
+
+questao.alternatives.forEach(alt => {
+alternativas += `<p>${alt.letter}) ${alt.text}</p>`
+})
+
+document.getElementById("questao").innerHTML =
+`
+<h2>Questão ${questao.index}</h2>
+<p>${questao.context}</p>
+
+<h3>Alternativas</h3>
+${alternativas}
+`
+
 }
